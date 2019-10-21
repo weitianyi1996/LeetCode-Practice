6a550e5a52862a656518eb51a7906f750218e22b
Tianyi Wei <43299961+weitianyi1996@users.noreply.github.com>
2019-10-20 21:03:03 -0400

    Add files via upload

--- 2020 Data Science Leetcode.py
+++ 2020 Data Science Leetcode.py
@@ -2465,28 +2465,46 @@ nums=[0,1]
 missingNumber(nums)
 
 
-# In[ ]:
-
-
-
-
+# ### 283. Move Zeroes
 
 # In[ ]:
 
 
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
 
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
 
 
+# In[233]:
 
+
+def moveZeros(nums):
+    """
+    :type nums: List[int]
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    i=0
+    zeronums=sum([1 for x in nums if x==0])
+    while i<len(nums)-zeronums:
+        if nums[i]==0:
+            del nums[i] #remove by element
+            nums.append(0)
+        else:
+            i+=1
+    return nums
+
+
+# In[234]:
+
+
+nums=[0,1,0,3,12]
+moveZeros(nums)
 
 
 # In[ ]:
