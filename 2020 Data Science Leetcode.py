26eefdca8deee6696b1ce19f969dba9026860042
Tianyi Wei <43299961+weitianyi1996@users.noreply.github.com>
2019-10-26 16:30:57 -0400

    Add files via upload

--- 2020 Data Science Leetcode.py
+++ 2020 Data Science Leetcode.py
@@ -2561,3 +2561,55 @@ def isPowerOfFour(num):
                 return False
         return True
 
+
+# ### 344. Reverse String
+
+# In[ ]:
+
+
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+
+# In[4]:
+
+
+def reverseString(s):
+    s[:]=s[::-1]
+    return s
+
+
+# In[23]:
+
+
+#recusion
+def reverseString(s):
+    if len(s)==1:
+        return s
+    else:
+        l=len(s)
+        return reverseString(s[l//2:])+reverseString(s[:l//2])
+
+
+# In[24]:
+
+
+s=["h","e","l","l","o"]
+reverseStringt(s)
+
+
+# In[ ]:
+
+
+
+
