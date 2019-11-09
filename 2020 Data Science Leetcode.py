60b7c85f3d29928c32f2325d5432711f0919d067
Tianyi Wei <43299961+weitianyi1996@users.noreply.github.com>
2019-11-08 23:24:03 -0500

    Add files via upload

--- 2020 Data Science Leetcode.py
+++ 2020 Data Science Leetcode.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# ### Array
+
 # ### Binary Search
 
 # ### Two Pointer
@@ -2638,4 +2640,55 @@ def firstBadversion(n):
         else:
             l=mid+1
     return l
+
+
+# ### 448. Find All Numbers Disappeared in an Array
+
+# Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+# 
+# Find all the elements of [1, n] inclusive that do not appear in this array.
+# 
+# Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+# 
+# Example:
+# 
+# Input:
+# [4,3,2,7,8,2,3,1]
+# 
+# Output:
+# [5,6]
+
+# In[25]:
+
+
+def findDisappeardNumbers(nums):
+    L=[]
+    for i,v in enumerate(nums):
+        if i+1!=v:
+            if i+1 not in nums:
+                L.append(i+1)
+    return L
+
+
+# In[66]:
+
+
+def findDisappeardNumbers(nums):
+    for i in range(len(nums)):
+        index=abs(nums[i])-1
+        nums[index]=-abs(nums[index])
+    return [i+1 for i in range(len(nums)) if nums[i]>0]
+
+
+# In[67]:
+
+
+nums=[4,3,2,7,8,2,3,1]
+findDisappeardNumbers(nums)
+
+
+# In[64]:
+
+
+abs(-13)
 
