df153120e9a5b5fe7502b73e5ae307db77be591d
weitianyi1996 <weitianyius1996@163.com>
2020-06-30 00:20:18 -0400

    update 53 maximum subarray dynamic programming solution-in development

---
+++ Algorithm/Array/118_Pascal's_triangle.py
@@ -0,0 +1,43 @@
+# 118. Pascal's Triangle
+# Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+#
+#
+# In Pascal's triangle, each number is the sum of the two numbers directly above it.
+#
+# Example:
+#
+# Input: 5
+# Output:
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],  d[2]
+#   [1,3,3,1], d[3]
+#  [1,4,6,4,1]
+# ]
+
+
+
+class Solution():
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        d = []
+        for i in range(0, numRows):
+            l = [1 for value in range(i+1)]
+            print(l)
+            if i >= 2:
+                last_time_list = [1, 1]
+                print(l)
+                for x in range(1, len(l)-1):
+                    l[x] = last_time_list[x-1]+last_time_list[x]
+                last_time_list = []
+                last_time_list.append(l)
+            d.append(l)
+        return d
+
+
+s1 = Solution()
+print(s1.generate(5))
