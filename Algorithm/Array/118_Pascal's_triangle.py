b9678d02026abe41a09b4f4bd1e1e6aba8b65ba0
weitianyi1996 <weitianyius1996@163.com>
2020-06-30 22:31:32 -0400

    update 188 pascal triangle solution

--- Algorithm/Array/118_Pascal's_triangle.py
+++ Algorithm/Array/118_Pascal's_triangle.py
@@ -17,7 +17,8 @@
 # ]
 
 
-class Solution():
+# wty solution
+class Solution1():
     def generate(self, numRows):
         """
         :type numRows: int
@@ -36,5 +37,17 @@ class Solution():
         return d
 
 
-s1 = Solution()
+class Solution2():
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        res = [[1]]
+        for i in range(1, numRows):
+            res += [list(map(lambda x,y: x+y,res[-1]+[0], [0]+res[-1]))]
+        return res[:numRows]  # solve for the numrows = 0
+
+
+s1 = Solution2()
 print(s1.generate(5))
